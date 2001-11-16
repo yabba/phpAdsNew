c713a57673fed2a1a20961942b68f0f12cf8f1ee
rakaz <rakaz>
2001-11-16 21:59:26 +0000

    Bugfix: withtext parameter should work again

--- adframe.php
+++ adframe.php
@@ -1,4 +1,4 @@
-<?php // $Revision: 1.7 $
+<?php // $Revision: 1.8 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
@@ -31,7 +31,7 @@ require("lib-cache.inc.php");
 
 
 if (isset($clientID) && !isset($clientid))	$clientid = $clientID;
-if (isset($withtext) && !isset($withText))  $withtext = $withText;
+if (isset($withText) && !isset($withtext))  $withtext = $withText;
 
 if (!isset($what)) 		$what = '';
 if (!isset($clientid)) 	$clientid = 0;
