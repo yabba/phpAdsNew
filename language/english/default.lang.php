444996c5f977575b8a7751d7d705d07c99f79280
rakaz <rakaz>
2001-11-25 16:54:41 +0000

    Added target based priority

--- language/english/default.lang.php
+++ language/english/default.lang.php
@@ -1,4 +1,4 @@
-<?php // $Revision: 1.9 $
+<?php // $Revision: 1.10 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
@@ -349,6 +349,7 @@ $GLOBALS['strLowPriority']				= "Show banner in this campaign with low priority.
 										   aren't used by high priority campaigns.";
 $GLOBALS['strTargetLimitAdviews']		= "Limit the number of AdViews to";
 $GLOBALS['strTargetPerDay']				= "per day.";
+$GLOBALS['strRecalculatePriority']		= "Recalculate priority";
 
 
 ?>
