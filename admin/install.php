ebf26839be74e5f422a3202359e10e89ac80e571
rakaz <rakaz>
2002-01-05 17:41:17 +0000

    Bugfix: new images table was not created during install

--- admin/install.php
+++ admin/install.php
@@ -1,4 +1,4 @@
-<?php // $Revision: 1.6 $
+<?php // $Revision: 1.7 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
@@ -237,6 +237,7 @@ if (phpAds_isUser(phpAds_Admin))
 				$installvars['tbl_zones'] 	 = $tbl_prefix.'zones';
 				$installvars['tbl_config'] 	 = $tbl_prefix.'config';
 				$installvars['tbl_affiliates'] 	 = $tbl_prefix.'affiliates';
+				$installvars['tbl_images'] 	 = $tbl_prefix.'images';
 				
 				// Go to next phase
 				$phase = 3;
