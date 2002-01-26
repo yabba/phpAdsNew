0dd2c9745e98fe8af3eb3d6b509c529f664a6afc
rakaz <rakaz>
2002-01-26 19:31:48 +0000

    Fixed image alignment

--- admin/affiliate-index.php
+++ admin/affiliate-index.php
@@ -1,4 +1,4 @@
-<?php // $Revision: 1.9 $
+<?php // $Revision: 1.10 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
@@ -255,7 +255,7 @@ else
 				// Icon & name
 				echo "<tr height='25' ".($i%2==0?"bgcolor='#F6F6F6'":"")."><td height='25'>";
 				echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-				echo "<img src='images/spacer.gif' height='16' width='16'>&nbsp;";
+				echo "<img src='images/spacer.gif' height='16' width='16' align='absmiddle'>&nbsp;";
 				
 				echo "<img src='images/icon-zone.gif' align='absmiddle'>&nbsp;";
 				
