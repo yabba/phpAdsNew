5543f25427ee21f5b64ec33af52f1323fa16fefb
rakaz <rakaz>
2002-01-28 20:18:28 +0000

    Bugfix: Changed version format in MySQL for 3.22 compatibility

--- lib-dbconfig.inc.php
+++ lib-dbconfig.inc.php
@@ -1,4 +1,4 @@
-<?php // $Revision: 1.13 $
+<?php // $Revision: 1.14 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
@@ -15,7 +15,7 @@
 
 
 // Current phpAds version
-$phpAds_version = 200.074;
+$phpAds_version = 200.075;
 $phpAds_version_readable = "2 dev 28012002";
 
 $phpAds_settings_information = array(
