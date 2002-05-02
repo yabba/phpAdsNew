b31c63a57d7a9b2bd5364951d4a3bf0830272f5a
rakaz <rakaz>
2002-05-02 00:03:44 +0000

    Bugfix: Moving zones now works again

--- admin/zone-modify.php
+++ admin/zone-modify.php
@@ -1,4 +1,4 @@
-<?php // $Revision: 1.4 $
+<?php // $Revision: 1.5 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
@@ -31,7 +31,7 @@ phpAds_checkAccess(phpAds_Admin);
 
 if (isset($zoneid) && $zoneid != '')
 {
-	if (isset($moveto_x) && $moveto != '')
+	if (isset($moveto) && $moveto != '')
 	{
 		// Move the zone
 		$res = phpAds_dbQuery("UPDATE ".$phpAds_config['tbl_zones']." SET affiliateid = '".$moveto."' WHERE zoneid = '".$zoneid."'") or phpAds_sqlDie();
