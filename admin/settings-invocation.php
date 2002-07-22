43742f226fca756014c3be511bfde61655c858f9
ciaccia <ciaccia>
2002-07-22 05:10:51 +0000

    Added missing zone cache limit error checking

--- admin/settings-invocation.php
+++ admin/settings-invocation.php
@@ -1,4 +1,4 @@
-<?php // $Revision: 1.9 $
+<?php // $Revision: 1.10 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
@@ -52,7 +52,12 @@ if (isset($HTTP_POST_VARS) && count($HTTP_POST_VARS))
 	if (isset($zone_cache))
 		phpAds_SettingsWriteAdd('zone_cache', $zone_cache);
 	if (isset($zone_cache_limit))
-		phpAds_SettingsWriteAdd('zone_cache_limit', $zone_cache_limit);
+	{
+		if (!is_numeric($zone_cache_limit) || $zone_cache_limit <= 0)
+			$errormessage[3][] = $strZoneCacheLimitErr;
+		else
+			phpAds_SettingsWriteAdd('zone_cache_limit', $zone_cache_limit);
+	}
 	
 	if (isset($p3p_policies))
 		phpAds_SettingsWriteAdd('p3p_policies', $p3p_policies);
