6c9f589549aa6e05c79cddaaa01e17f868fc0785
ciaccia <ciaccia>
2003-05-04 22:54:44 +0000

    Merged in from REL_2_0

--- language/indonesian/index.lang.php
+++ language/indonesian/index.lang.php
@@ -1,10 +1,10 @@
-<?php // $Revision: 1.1.2.3 $
+<?php // $Revision: 1.2 $
 
 /************************************************************************/
 /* phpAdsNew 2                                                          */
 /* ===========                                                          */
 /*                                                                      */
-/* Copyright (c) 2000-2003 by the phpAdsNew developers                  */
+/* Copyright (c) 2000-2002 by the phpAdsNew developers                  */
 /* For more information visit: http://www.phpadsnew.com                 */
 /*                                                                      */
 /* This program is free software. You can redistribute it and/or modify */
@@ -13,10 +13,10 @@
 /************************************************************************/
 
 
-// German
+
 // Meta information
-$translation_readable   = "Deutsch";
-$translation_maintainer	= "Rayner Jankowski";
-$translation_contact	= "info@Werbeplatzvermarktung.de";
+$translation_readable   = "Indonesian";
+$translation_maintainer	= "Rachim Tamsjadi";
+$translation_contact	= "tamsjadi@icqmail.com";
 
 ?>
